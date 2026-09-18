fd04e5130179b7f5ceb677f290fd50b6eafbf6bd
Frederick Willems <frederick@bravosix.eu>
2026-09-18 17:20:22 +0200

    fix voorwaarden container

--- resources/views/pages/voorwaarden.blade.php
+++ resources/views/pages/voorwaarden.blade.php
@@ -6,7 +6,9 @@
 @section('content')
     <x-page-hero kicker="Juridisch" title="Algemene voorwaarden" intro="Bestel- en leveringsvoorwaarden van Ladies’ Circle Dendermonde." />
 
-    <article class="prose-club mx-auto max-w-3xl px-4 py-16 sm:px-6">
-        {!! \Illuminate\Support\Str::markdown(\Illuminate\Support\Facades\File::get(resource_path('content/pages/voorwaarden.md'))) !!}
+    <article class="mx-auto max-w-6xl px-4 py-16 sm:px-6">
+        <div class="prose-club">
+            {!! \Illuminate\Support\Str::markdown(\Illuminate\Support\Facades\File::get(resource_path('content/pages/voorwaarden.md'))) !!}
+        </div>
     </article>
 @endsection
